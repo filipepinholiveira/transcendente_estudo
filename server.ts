d881912cc8e685f0d1e3ae51a494aa88b34afcb7
filipepinholiveira <filipepinholiveira@gmail.vom>
2025-05-08 17:37:17 +0100

    video zuca sobre criar site 12:30

--- server.ts
+++ server.ts
@@ -3,9 +3,12 @@ import path from 'path';
 
 const app = express();
 
+// Servir arquivos estáticos da pasta 'public' (ou onde estiver seu CSS)
+app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
+
 // Serve o arquivo index.html da raiz do projeto
-app.get('/', (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('index.html'));
 });
 
 // Servir o CSS gerado pelo Tailwind da pasta 'dist'
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 // tailwind.config.js
 module.exports = {
   darkMode: 'class', // Defina 'class' para que a classe 'dark' seja adicionada manualmente
-  content: ["./src/**/*.{html,js}"], // Verifique se o caminho está correto
+  content: ["./**/*.{html,js}"], // Verifique se o caminho está correto
   theme: {
     extend: {},
   },
